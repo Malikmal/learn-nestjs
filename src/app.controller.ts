0b73e34309d744a81009c8e87130fe165dcf893c
ikmalil birri <malikmal98@gmail.com>
2022-03-30 19:41:47 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, HttpCode, HttpException, HttpStatus, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Request } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@Req() request: Request): any {
+    // return request;
+    console.log(request.query);
+    return this.appService.getHello();
+  }
+
+  @Get('/test/:id')
+  getTest(@Req() request: Request): any {
+    // throw new HttpException('wkwkwk', HttpStatus.NOT_FOUND);
+    throw new HttpException({
+      message: 'asdlaksdasd',
+      success: false,
+      status: HttpStatus.FORBIDDEN,
+      error: 'This is a custom message',
+    }, HttpStatus.NOT_FOUND);
+    console.log(request.params);
+    return 'hello worldsslsld';
+  }
+}
