2543a00532ee0e7cbee1a47d48f9ba803455e8dc
ikmalil birri <malikmal98@gmail.com>
2023-02-01 00:12:40 +0700

    enhancement: logger - learn middleware make logger middleware to save anything manipulation data logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './logger.middleware';
@@ -11,6 +16,17 @@ import { TodosModule } from './todos/todo.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('todo');
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: 'todo',
+      method: RequestMethod.POST,
+    });
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: 'todo/*',
+      method: RequestMethod.PUT,
+    });
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: 'todo/*',
+      method: RequestMethod.DELETE,
+    });
   }
 }
